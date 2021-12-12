645c2557dc498d7b73e98335dc10c203d603032f
Andrey <andrewboyko_91@mail.ru>
2021-12-12 12:22:32 +0300

    add file FirstHomework.java

---
+++ FirstHomework.java
@@ -0,0 +1,49 @@
+//
+//	Home work #1
+//
+//	Andrei Boiko
+//
+class FirstHomework {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        
+    }
+        static void printThreeWords() {
+            System.out.println("1)----------");
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+        }
+        
+        static void checkSumSign() {
+            int a = 11;
+            int b = -10;
+            
+                System.out.println("2)----------");
+                System.out.println("Sum is " +(a + b >= 0? "positive" : "negative"));
+        }
+        
+        static void printColor() {
+            int value = 101;
+                System.out.println("3)----------");
+            if (value <= 0) {
+                System.out.println("Color is Red");
+            }
+            else if (value <= 100) {
+                System.out.println("Color is Yellow");
+            }
+            else {
+                System.out.println("Color is Green");
+            }
+        }
+        static void compareNumbers() {
+            int a = 1;
+            int b = 2;
+                System.out.println("4)----------");
+                System.out.println("Result is " +(a >= b? "a >= b" : "a < b"));
+        }
+            
+}
