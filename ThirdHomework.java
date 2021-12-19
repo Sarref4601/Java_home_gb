0a549b04cb1198312df1baf0ae8b4262544497be
Andrey <andrewboyko_91@mail.ru>
2021-12-19 14:35:17 +0300

    update file ThirdHomework.java

--- ThirdHomework.java
+++ ThirdHomework.java
@@ -12,45 +12,38 @@ class ThirdHomework {
         increase0To100();
         checkArrayLower6();
         useDiagonal();
-        System.out.println("5---------------------------");
         System.out.println(Arrays.toString(isReturnArray(10, 13)));
     }
     static void reverse0To1() {
         int[] arr = {1, 0, 1, 0, 0, 1};
-                System.out.println ("1--------------------------");
             for (int i = 0; i < arr.length; i++) {
-                System.out.println(arr[i] == 0? arr[i] + 1 : arr[i] - 1);
+                arr[i] = 1 - arr[i];
             }
+                System.out.println(Arrays.toString(arr));
     }
     static void increase0To100() {
-                System.out.println ("2---------------------------");
         int[] arr = new int[101];
             for (int i = 0; i < arr.length; i++) {
-                arr[0] = 0;
+                arr[i] = i;
             }
-            for (int i = 0; i < arr.length; i++) {;
-                System.out.print(arr[i] + i +" ");
-            }
-                System.out.println();
+                System.out.println(Arrays.toString(arr));
     }
     static void checkArrayLower6() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-                    System.out.println ("3---------------------------");
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] < 6) {
-                    System.out.print(arr[i] * 2 + " ");
+                    arr[i] = arr[i] * 2;
                 } else {
-                    System.out.print(arr[i] + " ");
+                    arr[i] = arr[i];
                 }
             }
-                    System.out.println();
+                    System.out.println(Arrays.toString(arr));
     }
     static void useDiagonal() {
         int[][] arr = new int[4][4];
-                            System.out.println ("4---------------------------");
             for (int i = 0; i < arr.length; i++) {
                 for (int j = 0; j < arr.length; j++) {
-                arr[i][j] = 2;
+                arr[i][j] = i;
                 }
             }
                 for (int i = 0; i < arr.length; i++) {
@@ -58,7 +51,7 @@ class ThirdHomework {
                         if (i > j || i < j) {
                             System.out.print(arr[i][j] + " ");
                         } else {
-                            System.out.print(arr[i][j] - 1 + " ");
+                            System.out.print("1 ");
                         }
                     }
                             System.out.println();
